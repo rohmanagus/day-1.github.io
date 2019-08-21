f7053b6215c513bb8115da5eac0e335291af7dc3
rohmanagus <52437354+rohmanagus@users.noreply.github.com>
2019-08-21 22:09:16 +0700

    WeekOne

---
+++ exercises-9/Exercises-9-2.js
@@ -0,0 +1,8 @@
+function calculateMultiply() {
+    return num1 * num2;
+}
+var num1 = 5;
+var num2 = 6;
+
+var hasilPerkalian = calculateMultiply(num1, num2);
+console.log(hasilPerkalian); // Menampilkan angka 30
---
+++ exercises-9/Exercises-9-3.js
@@ -0,0 +1,11 @@
+function processSentence() {
+    return 'nama saya  ' + name + ', Umur saya  ' + age + ', Alamat saya  ' + address + ', hoby saya  ' + hobby;
+}
+
+var name = "Agus";
+var age = 30;
+var address = "Jln. Malioboro, Yogjakarta";
+var hobby = "gaming";
+
+var fullSentence = processSentence(name, age, address, hobby);
+console.log(fullSentence); // Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya di Jln. Malioboro, Yogjakarta, dan saya punya hobby yaitu gaming!"
