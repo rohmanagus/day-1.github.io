478500a123453649fd8c33690fde90bf617f6a21
rohmanagus <52437354+rohmanagus@users.noreply.github.com>
2019-08-23 23:35:45 +0700

    weekOne | Day-2

---
+++ exercises-3/dataHandling.js
@@ -0,0 +1,33 @@
+//Buatlah sebuah fungsi dengan nama dataHandling dengan sebuah parameter untuk menerima argumen. Argumen yang akan diterima adalah sebuah array yang berisi beberapa array sejumlah n. Contoh input dapat dilihat dibawah:
+
+ar bawah = '';
+
+function dataHandling(argumen) {
+    for (var i = 0; i <= argumen.length - 1; i++) { //menambahkan kebawah
+        bawah;
+        for (j = 0; j <= i; j++) { //menambahkan kesamping
+
+        }
+
+        console.log('Nomor ID :' + argumen[i][0]);
+        console.log('Nama kamu :' + argumen[i][1]);
+        console.log('TTL :' + argumen[i][2]);
+        console.log('Hobi :' + argumen[i][3])
+        console.log('');
+    }
+    return bawah;
+}
+
+console.log(dataHandling([
+    ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+    ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+    ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+    ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+]))
+
+var input = [
+    ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+    ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+    ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+    ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+]
