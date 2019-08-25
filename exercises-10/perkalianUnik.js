6b968307b65eb257849cdbe4cc9d0c90f4eeff90
rohmanagus <52437354+rohmanagus@users.noreply.github.com>
2019-08-25 17:37:30 +0700

    weekOne | Day -2

---
+++ exercises-10/perkalianUnik.js
@@ -0,0 +1,24 @@
+// Diberikan sebuah function perkalianUnik(arr) yang menerima satu parameter berupa array yang berisikan angka. Function akan me-return array baru yang mengandung angka yang setiap nilainya merupakan hasil kali angka lain yang bukan angka itu sendiri. Contoh, jika arr adalah [1, 2, 3, 4, 5], maka function akan mereturn [120, 60, 40, 30, 24], karena 120 adalah 2 * 3 * 4 * 5, 60 adalah 1 * 3 * 4 * 5, 40 adalah 1 * 2 * 4 * 5, dan seterusnya.
+
+
+function perkalianUnik(arr) {
+    var jml = []; //tambahkan variabel baru(bebas)
+    for (var i = 0; i < arr.length; i++) { //perulangan menggunakan for
+        var tetap = 1; //tambah variabel baru
+        for (var j = 0; j < arr.length; j++) { //perulangan for (kedua)
+            if (j !== i) { //kondisi jika nilai J tidak sama dengan i
+                //rumus
+                tetap *= arr[j];
+            }
+        }
+        jml.push(tetap);
+    }
+    return jml;
+}
+
+// TEST CASES
+console.log(perkalianUnik([2, 4, 6])); // [24, 12, 8]
+console.log(perkalianUnik([1, 2, 3, 4, 5])); // [120, 60, 40, 30, 24]
+console.log(perkalianUnik([1, 4, 3, 2, 5])); // [120, 30, 40, 60, 24]
+console.log(perkalianUnik([1, 3, 3, 1])); // [9, 3, 3, 9]
+console.log(perkalianUnik([2, 1, 8, 10, 2])); // [160, 320, 40, 32, 160]
